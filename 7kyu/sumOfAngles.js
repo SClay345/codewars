409b93ed1756f38b46cc7854890d368a59fa131f
Clay Schneider <schneiderclay92@gmail.com>
2022-04-01 22:03:34 -0700

    added katas done on 4/1/22

---
+++ 7kyu/sumOfAngles.js
@@ -0,0 +1,8 @@
+//7 Kyu
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+
+// My Solution
+function angle(n) {
+    return (n-2)*180
+  }
