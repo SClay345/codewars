dcab984e509a1978bfb66be4d2a9cb8ba1c01c6a
Clay Schneider <schneiderclay92@gmail.com>
2022-04-13 20:39:14 -0700

    Added Katas done on 4/13/22

---
+++ 7kyu/sumOfTheNthTermOfSeries.js
@@ -0,0 +1,36 @@
+// // 7 Kyu
+// Task:
+
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+// Rules:
+
+//     You need to round the answer to 2 decimal places and return it as String.
+
+//     If the given value is 0 then it should return 0.00
+
+//     You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+// My Solution
+function SeriesSum(n) {
+  let result = 0;
+  let count = 1;
+
+  for (i = 0; i < n; i++) {
+    if (i === 0) {
+      result = 1;
+    } else {
+      count += 3;
+      result = result + 1 / count;
+    }
+  }
+  return result.toFixed(2);
+}
