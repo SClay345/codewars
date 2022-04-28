2556d4d11d98b2e0d9941239857668158ba1a738
Clay Schneider <schneiderclay92@gmail.com>
2022-04-27 23:47:09 -0700

    added katas done on 4/27/22

---
+++ 8Kyu/getCharFromValue.js
@@ -0,0 +1,18 @@
+//8 Kyu
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+
+// should return:
+
+// 'A'
+
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+
+// My Solution
+function getChar(c){
+    return String.fromCharCode(c)
+  }
