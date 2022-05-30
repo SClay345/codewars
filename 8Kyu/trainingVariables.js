d237f6d44a69c799e33753af5dad5f0c9b1d20d2
Clay Schneider <schneiderclay92@gmail.com>
2022-05-30 16:13:04 -0700

    added katas done on 5/30/22

---
+++ 8Kyu/trainingVariables.js
@@ -0,0 +1,11 @@
+// 8 Kyu
+
+// Variable assignment
+
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// My Solution
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
