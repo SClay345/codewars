a58fe815037d05b6787c98c0b220173b7851e11f
Clay Schneider <schneiderclay92@gmail.com>
2022-06-01 16:40:52 -0700

    added katas done on 6/1/22

---
+++ 8Kyu/return5.js
@@ -0,0 +1,12 @@
+// 8 Kyu
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+
+// my Solution
+function unusualFive() {
+    return "hello".length
+   }
