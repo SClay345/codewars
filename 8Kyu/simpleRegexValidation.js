ecbf45b241e27169ef928f45704980c956f9183c
Clay Schneider <schneiderclay92@gmail.com>
2022-07-21 15:56:52 -0700

    added katas done on 7/21/22

---
+++ 8Kyu/simpleRegexValidation.js
@@ -0,0 +1,15 @@
+// Write a simple regex to validate a username. Allowed characters are:
+
+//     lowercase letters,
+//     numbers,
+//     underscore
+
+// Length should be between 4 and 16 characters (both included).
+
+
+
+// My Solution
+function validateUsr(username) {
+    res =  /^[a-z0-9_]{4,16}$/.test(username) 
+    return res
+  }
